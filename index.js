fbc947a01efbd21df9ea63f6801e84bf1dcb63d8
Shadrack5 <shadrackkibet100@gmail.com>
2023-06-14 20:12:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+      console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return thankYouMessages;
+  }
+  
+  writeCards(names);
+  
+  function countDown(number) {
+    let i= number;
+    while (i >= 0) {
+      console.log(i); i--;
+    }
+    return number;
+  }
+  countDown(10)
